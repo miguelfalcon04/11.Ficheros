b6f39379e27bc6011c1ceb73c91b27b07a29fef9
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-02-15 19:55:16 +0100

    commit

---
+++ Eje04.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Eje04 {
+
+    public static void ordenaPalabras(File f){
+        
+    }
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("fichEje04.txt"));
+            BufferedWriter bf = new BufferedWriter(new FileWriter("fichEje04.txt"));
+
+            
+
+            
+        } catch (IOException e) {
+            System.out.println("Erro repentino");
+        }
+        
+    }
+}
